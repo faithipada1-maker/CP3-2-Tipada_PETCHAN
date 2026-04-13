41bbc2750751035fa0691c9bfa3db2ef9e86435c
faithipada1-maker <faithipada1@gmail.com>
2026-04-13 21:11:25 +0700

    Update Exercise5_1_Tipada_P.py

--- Exercise5_1_Tipada_P.py
+++ Exercise5_1_Tipada_P.py
@@ -4,5 +4,7 @@ result1 = num1 + num2
 result2 = num1 - num2
 result3 = num1*num2
 result4 = num1/num2
-print(result4)
-
+print(num1,"+",num2,"=",result1)
+print(num1,"+",num2,"=",result2)
+print(num1,"+",num2,"=",result3)
+print(num1,"+",num2,"=",result4)
