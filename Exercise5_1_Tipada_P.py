1e284a9107251f5740935aa7201f1974c48ecd9e
faithipada1-maker <faithipada1@gmail.com>
2026-04-13 11:02:25 +0700

    update

---
+++ Exercise5_1_Tipada_P.py
@@ -0,0 +1,8 @@
+num1 = int(input("num1 :"))
+num2 = int(input("num2 :"))
+result1 = num1 + num2
+result2 = num1 - num2
+result3 = num1*num2
+result4 = num1/num2
+print(result4)
+
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-print("")
+print("hlll")
