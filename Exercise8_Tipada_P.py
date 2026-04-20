db0fcb01a3006238542f80820b0a01754f2c82f8
faithipada1-maker <faithipada1@gmail.com>
2026-04-20 22:38:42 +0700

    Update Exercise8_Tipada_P.py

--- Exercise8_Tipada_P.py
+++ Exercise8_Tipada_P.py
@@ -6,5 +6,16 @@ if userName == "Tipada" and password == "fai123":
     print("1.milk 20 BAHT")
     print("2.bread 15 BAHT")
     print("3.Yogurt 12 BAHT")
-userSelect = int(input(">>"))
+    userSelect1 = int(input("How manybox of milk do you want : "))
+    userSelect2 = int(input("How many bread do you want :"))
+    userSelect3 = int(input("How many yogurt do you want :"))
+    result = (userSelect1*20) + (userSelect2*15) + (userSelect3 *12)
+    print(result,"Baht")
+elif userName != "Tipada" or password !=  "fai123":
+    print("ERROR!")
+    print("Please try again")
+
+
+
+
 
