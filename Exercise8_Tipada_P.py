8424049303a45c6e3b6e5c43c5b7f5a6051d0e2c
faithipada1-maker <faithipada1@gmail.com>
2026-04-20 09:58:39 +0700

    Update Exercise8_Tipada_P.py

--- Exercise8_Tipada_P.py
+++ Exercise8_Tipada_P.py
@@ -1,2 +1,10 @@
-userName = Tipada
-Password = fai1
+userName = input("Username :")
+password = input("Password:")
+if userName == "Tipada" and password == "fai123":
+    print("DONE!")
+    print("---SHOP---")
+    print("1.milk 20 BAHT")
+    print("2.bread 15 BAHT")
+    print("3.Yogurt 12 BAHT")
+userSelect = int(input(">>"))
+
